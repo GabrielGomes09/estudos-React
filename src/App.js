bdaabdb99419ac74d9dc4d15b45cdec36cc41af0
GabrielGomes09 <gadigomes09@gmail.com>
2026-03-31 11:02:11 -0300

    Tasklist em React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [tarefas, setTarefas] = useState([
+    "Pagar a conta de luz",
+    "Estudar React JS",
+  ]);
+
+  useEffect(() => {
+    const tarefasStorage = localStorage.getItem("@tarefas");
+
+    if (tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("@tarefas", JSON.stringify(tarefas));
+  }, [tarefas]);
+
+  function handleRegister(e) {
+    e.preventDefault();
+
+    setTarefas([...tarefas, input]);
+    setInput("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TaskList</h1>
+
+      <form onSubmit={handleRegister}>
+        <label>Nova tarefa:</label>
+        <br />
+        <input
+          placeholder="Digite a tarefa"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <br />
+        <br />
+
+        <button type="submit">Registrar</button>
+      </form>
+      <br />
+
+      <ul>
+        {tarefas.map((tarefa) => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/components/Nome.js
@@ -0,0 +1,5 @@
+function Nome(props) {
+  return <span>Bem-vindo, {props.nome}!</span>;
+}
+
+export default Nome;
